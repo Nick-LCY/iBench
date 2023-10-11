080375a54737a18774ba0daef27fe41f996c4b22
NickLin <nicklin9907@gmail.com>
2023-10-11 11:18:20 +0800

    Container compatibility

--- src/memCap.c
+++ src/memCap.c
@@ -50,37 +50,43 @@ void remove_all_chars(char* str, char c) {
 }
 
 long long int memory_size_kb(void) {
-	char line[512], buffer[32];
+	char line[512];
 	long long int column;
 	FILE *meminfo;
 
-
-	if (!(meminfo = fopen("/proc/meminfo", "r"))) {
-		perror("/proc/meminfo: fopen");
-		return -1;
-	}
-
-
-	while (fgets(line, sizeof(line), meminfo)) {
-		if (strstr(line, "MemTotal")) {
-			char* colStr;
-			colStr = strstr(line, ":");
-			remove_all_chars(colStr, ':'); 
-			remove_all_chars(colStr, 'k'); 
-			remove_all_chars(colStr, 'B');
-			remove_all_chars(colStr, ' ');
-			column = atoi(colStr);
-		        column = 1000*column;	
-			fclose(meminfo);
-			return column; 
+	if (!(meminfo = fopen("/sys/fs/cgroup/memory.max", "r"))) {
+		perror("/sys/fs/cgroup/memory.max: fopen");
+		if (!(meminfo = fopen("/sys/fs/cgroup/memory/memory.limit_in_bytes", "r"))) {
+			perror("/sys/fs/cgroup/memory/memory.limit_in_bytes: fopen");
+			if (!(meminfo = fopen("/proc/meminfo", "r"))) {
+				perror("/proc/meminfo: fopen");
+				return -1;
+			}
+			while (fgets(line, sizeof(line), meminfo)) {
+				if (strstr(line, "MemTotal")) {
+					char* colStr;
+					colStr = strstr(line, ":");
+					remove_all_chars(colStr, ':'); 
+					remove_all_chars(colStr, 'k'); 
+					remove_all_chars(colStr, 'B');
+					remove_all_chars(colStr, ' ');
+					column = atoi(colStr);
+					column = 1000*column;	
+					fclose(meminfo);
+					return column; 
+				}
+			}
 		}
 	}
+
+	fgets(line, sizeof(line), meminfo);
+	column = atoll(line);
 	fclose(meminfo);
-	return -1;
+	return column;
 }
 
 int main(int argc, char **argv) {
-	timespec sleepValue = {0};
+	struct timespec sleepValue = {0};
 
 	char* volatile block;
 	long long int MEMORY_SIZE = memory_size_kb(); 
@@ -94,18 +100,12 @@ int main(int argc, char **argv) {
 	block = (char*)mmap(NULL, MEMORY_SIZE, PROT_READ | PROT_WRITE, MAP_PRIVATE | MAP_ANONYMOUS, 0, 0);
 
 	int usr_timer = atoi(argv[1]);
-	double time_spent = 0.0; 
-  	clock_t begin, end;
 
-
-	while (time_spent < usr_timer) {
-  		begin = clock();
-		memcpy(block, block+MEMORY_SIZE/2, MEMORY_SIZE/2);
-		//sleepValue.tv_nsec = (usr_timer-getNs())/usr_timer;
-		//nanosleep(&sleepValue, NULL);
-		end = clock();
-  		time_spent += (double)(end - begin) / CLOCKS_PER_SEC;
-	}
+	int capacity;
+	// To avoid OOM, only occupy 95% of RAM
+	capacity = (int) MEMORY_SIZE * 0.95;
+	memcpy(block, block+1, capacity);
+	sleep(usr_timer);
 	return 0;
 }
 
